f261e7685c756e1f0a3c11234c9b0d5f38fb9b4d
Michele Caini <michele.caini@gmail.com>
2016-04-16 17:22:16 +0200

    renaming

--- src/bus.hpp
+++ src/bus.hpp
@@ -10,13 +10,13 @@ namespace eventpp {
 namespace details {
 
 template<class E>
-struct event_tag { using type = E; };
+struct ETag { using type = E; };
 
 template<int N, int M>
-struct choice: public choice<N+1, M> { };
+struct Choice: public Choice<N+1, M> { };
 
 template<int N>
-struct choice<N, N> { };
+struct Choice<N, N> { };
 
 }
 
@@ -35,15 +35,15 @@ protected:
     using Base::get;
     using Base::reg;
 
-    Signal<E>& get(details::event_tag<E>) {
+    Signal<E>& get(details::ETag<E>) {
         return signal;
     }
 
     template<class C>
-    auto reg(details::choice<S-(sizeof...(O)+1), S>, std::weak_ptr<C> ptr)
+    auto reg(details::Choice<S-(sizeof...(O)+1), S>, std::weak_ptr<C> ptr)
         -> decltype(std::declval<C>().receive(std::declval<E>())) {
         signal.template add<C, &C::receive>(ptr);
-        Base::reg(details::choice<S-sizeof...(O), S>{}, ptr);
+        Base::reg(details::Choice<S-sizeof...(O), S>{}, ptr);
     }
 
     std::size_t size() const noexcept {
@@ -59,7 +59,7 @@ class BusBase<S> {
 protected:
     virtual ~BusBase() { }
     void get();
-    void reg(details::choice<S, S>, std::weak_ptr<void>) { }
+    void reg(details::Choice<S, S>, std::weak_ptr<void>) { }
     std::size_t size() const noexcept { return 0; }
 };
 
@@ -72,7 +72,7 @@ public:
 
     template<class E, void(*F)(const E &)>
     void add() {
-        Signal<E> &signal = Base::get(details::event_tag<E>{});
+        Signal<E> &signal = Base::get(details::ETag<E>{});
         signal.template add<F>();
     }
 
@@ -80,34 +80,34 @@ public:
     typename std::enable_if<std::is_convertible<P<C>, std::weak_ptr<C>>::value, void>::type
     reg(P<C> &ptr) {
         auto wptr = static_cast<std::weak_ptr<C>>(ptr);
-        Base::reg(details::choice<0, sizeof...(T)>{}, wptr);
+        Base::reg(details::Choice<0, sizeof...(T)>{}, wptr);
     }
 
     template<class E, class C, void(C::*M)(const E &) = &C::receive, template<typename> class P>
     typename std::enable_if<std::is_convertible<P<C>, std::weak_ptr<C>>::value, void>::type
     add(P<C> &ptr) {
-        Signal<E> &signal = Base::get(details::event_tag<E>{});
+        Signal<E> &signal = Base::get(details::ETag<E>{});
         auto wptr = static_cast<std::weak_ptr<C>>(ptr);
         signal.template add<C, M>(wptr);
     }
 
     template<class E, void(*F)(const E &)>
     void remove() {
-        Signal<E> &signal = Base::get(details::event_tag<E>{});
+        Signal<E> &signal = Base::get(details::ETag<E>{});
         signal.template remove<F>();
     }
 
     template<class E, class C, void(C::*M)(const E &) = &C::receive, template<typename> class P>
     typename std::enable_if<std::is_convertible<P<C>, std::weak_ptr<C>>::value, void>::type
     remove(P<C> &ptr) {
-        Signal<E> &signal = Base::get(details::event_tag<E>{});
+        Signal<E> &signal = Base::get(details::ETag<E>{});
         auto wptr = static_cast<std::weak_ptr<C>>(ptr);
         signal.template remove<C, M>(wptr);
     }
 
     template<class E, class... A>
     void publish(A&&... args) {
-        Signal<E> &signal = Base::get(details::event_tag<E>{});
+        Signal<E> &signal = Base::get(details::ETag<E>{});
         E e(std::forward<A>(args)...);
         signal.publish(e);
     }
